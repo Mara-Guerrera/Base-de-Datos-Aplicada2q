c72500e576285f170b9d749b27db8cdbbb189544
Pilubou <labrujacosmica1970@gmail.com>
2024-11-07 20:34:17 -0300

    Update and rename Intento.sql to Importacion linea y categoria.sql

--- SQL/Importacion linea y categoria.sql
+++ SQL/Importacion linea y categoria.sql
@@ -1,19 +1,19 @@
 /*Se proveen los archivos en el TP_integrador_Archivos.zip
-Ver archivo ìDatasets para importarî en Miel.
-Se requiere que importe toda la informaciÛn antes mencionada a la base de datos:
-ï Genere los objetos necesarios (store procedures, funciones, etc.) para importar los
-archivos antes mencionados. Tenga en cuenta que cada mes se recibir·n archivos de
+Ver archivo ‚ÄúDatasets para importar‚Äù en Miel.
+Se requiere que importe toda la informaci√≥n antes mencionada a la base de datos:
+‚Ä¢ Genere los objetos necesarios (store procedures, funciones, etc.) para importar los
+archivos antes mencionados. Tenga en cuenta que cada mes se recibir√°n archivos de
 novedades con la misma estructura, pero datos nuevos para agregar a cada maestro.
-ï Considere este comportamiento al generar el cÛdigo. Debe admitir la importaciÛn de
-novedades periÛdicamente.
-ï Cada maestro debe importarse con un SP distinto. No se aceptar·n scripts que
+‚Ä¢ Considere este comportamiento al generar el c√≥digo. Debe admitir la importaci√≥n de
+novedades peri√≥dicamente.
+‚Ä¢ Cada maestro debe importarse con un SP distinto. No se aceptar√°n scripts que
 realicen tareas por fuera de un SP.
-ï La estructura/esquema de las tablas a generar ser· decisiÛn suya. Puede que deba
-realizar procesos de transformaciÛn sobre los maestros recibidos para adaptarlos a la
+‚Ä¢ La estructura/esquema de las tablas a generar ser√° decisi√≥n suya. Puede que deba
+realizar procesos de transformaci√≥n sobre los maestros recibidos para adaptarlos a la
 estructura requerida.
-ï Los archivos CSV/JSON no deben modificarse. En caso de que haya datos mal
-cargados, incompletos, errÛneos, etc., deber· contemplarlo y realizar las correcciones
-en el fuente SQL. (SerÌa una excepciÛn si el archivo est· malformado y no es posible
+‚Ä¢ Los archivos CSV/JSON no deben modificarse. En caso de que haya datos mal
+cargados, incompletos, err√≥neos, etc., deber√° contemplarlo y realizar las correcciones
+en el fuente SQL. (Ser√≠a una excepci√≥n si el archivo est√° malformado y no es posible
 interpretarlo como JSON o CSV).*/
 
 sp_configure 'show advanced options', 1;
@@ -27,14 +27,14 @@ GO
 SELECT * INTO #TempImport
 FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0', 
                 'Excel 12.0;Database=C:\Users\Public\Downloads\TP_integrador_Archivos\Informacion_complementaria.xlsx; HDR=YES', 
-                'SELECT [LÌnea de producto], [Producto] FROM [Clasificacion Productos$B:C]');
+                'SELECT [L√≠nea de producto], [Producto] FROM [Clasificacion Productos$B:C]');
 
 
 -- Insertar los datos en TipoProducto
 INSERT INTO gestion_productos.TipoProducto (nombre)
-SELECT DISTINCT [LÌnea de producto]
+SELECT DISTINCT [L√≠nea de producto]
 FROM #TempImport
-WHERE [LÌnea de producto] IS NOT NULL;
+WHERE [L√≠nea de producto] IS NOT NULL;
 
 SELECT *
 FROM gestion_productos.TipoProducto
@@ -44,7 +44,7 @@ SELECT
     Tabla.[Producto],
     Linea.id
 FROM #TempImport Tabla JOIN gestion_productos.TipoProducto Linea 
-ON Tabla.[LÌnea de producto] = Linea.nombre COLLATE Modern_Spanish_CI_AI
+ON Tabla.[L√≠nea de producto] = Linea.nombre COLLATE Modern_Spanish_CI_AI
 WHERE 
     Tabla.[Producto] IS NOT NULL;
 
