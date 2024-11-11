6b19cc71c96fd6a2784f08ef5d487f6de90f75ae
Pilubou <labrujacosmica1970@gmail.com>
2024-11-11 13:46:19 -0300

    Update Ventas.sql

--- SQL/Ventas.sql
+++ SQL/Ventas.sql
@@ -1,69 +1,153 @@
-
---sp_configure 'show advanced options', 1;
---RECONFIGURE;
---GO
---sp_configure 'Ad Hoc Distributed Queries', 1;
---RECONFIGURE;
---GO
-
-
+/*sp_configure 'show advanced options', 1;
+RECONFIGURE;
+GO
+sp_configure 'Ad Hoc Distributed Queries', 1;
+RECONFIGURE;
+GO*/
+USE Com5600G05
+GO
 CREATE OR ALTER PROCEDURE Importar_ventas_csv
 @RutaArchivo NVARCHAR(400)
 AS
 BEGIN
+
 	DECLARE @Dinamico NVARCHAR(max)
-	IF OBJECT_ID('tempdb..##TempVentas') IS NOT NULL
+	IF OBJECT_ID('tempdb..#TempVentas') IS NOT NULL
     BEGIN
-        DROP TABLE ##TempVentas;
+        DROP TABLE #TempVentas;
     END
 
-	CREATE TABLE ##TempVentas
+	CREATE TABLE #TempVentas
     (
-        id_factura NVARCHAR(20),
+        id_factura VARCHAR(20),
         tipo_factura VARCHAR(5),
         ciudad VARCHAR(50),
         tipo_cliente VARCHAR(20),
         genero VARCHAR(10),
-        producto NVARCHAR(100),
+        producto VARCHAR(100),
         precio_unitario DECIMAL(10, 2),
         cantidad INT,
-        fecha DATE,
-        hora TIME(7),
+        fecha VARCHAR(20),  -- Formato dd/mm/aa
+        hora VARCHAR(20),
         medio_pago VARCHAR(20),
         empleado INT,
         identificador_pago VARCHAR(50)
     );
 
-	SET @Dinamico = 'BULK INSERT ##TempVentas
+	SET @Dinamico = 'BULK INSERT #TempVentas
 	FROM ''' + @RutaArchivo + ''' WITH
 	(
-		FORMAT = ''CSV'',
-		FIELDTERMINATOR = '';'', 
-		ROWTERMINATOR = ''0x0a'', 
-		CODEPAGE = ''65001'', 
+		CODEPAGE = ''65001'',
+		FIELDTERMINATOR = '';'',
+		ROWTERMINATOR = ''0x0a'',
 		FIRSTROW = 2
 	);'
 
 	exec sp_executesql @Dinamico; 
 
-	INSERT INTO gestion_venta.Factura(id, fecha, hora, id_medioDePago)
-		SELECT 
-		v.id_factura AS id,
-		v.fecha AS fecha,
-		v.hora AS hora,
-		m.id AS id_medioDePago
-		FROM ##TempVentas AS v
-		inner join gestion_venta.MedioDePago AS m on m.nombre = v.medio_pago  COLLATE MODERN_SPANISH_CI_AS;
+	--SELECT * FROM #TempVentas
+	--Inserción de tipos de factura-- Revisar si es lo más óptimo (hay 1000 registros)
+	INSERT INTO gestion_venta.TipoFactura(nombre)
+	SELECT DISTINCT tipo_factura
+	FROM #TempVentas AS v
+	WHERE NOT EXISTS (
+		SELECT 1 
+		FROM gestion_venta.TipoFactura AS t
+		WHERE t.nombre = v.tipo_factura
+	);
 
-	INSERT INTO gestion_venta.DetalleVenta(id_factura, cantidad, subtotal, precio_unitario)
-		SELECT 
-		v.id_factura AS id_factura,
-		v.cantidad AS cantidad,
-		(v.precio_unitario * v.cantidad) as subtotal,
-		v.precio_unitario as precio_unitario
-		FROM ##TempVentas AS v
+	UPDATE #TempVentas
+	SET producto = REPLACE(producto, 'Ã¡', 'á')
+	WHERE producto LIKE '%Ã¡%';
 
-	--DROP TABLE ##TempVentas;
+	UPDATE #TempVentas
+	SET producto = REPLACE(producto, 'Ã©', 'é')
+	WHERE producto LIKE '%Ã©%';
+
+	UPDATE #TempVentas
+	SET producto = REPLACE(producto, 'Ã³', 'ó')
+	WHERE producto LIKE '%Ã³%';
+
+	UPDATE #TempVentas
+	SET producto = REPLACE(producto, 'Ãº', 'ú')
+	WHERE producto LIKE '%Ãº%';
+
+	UPDATE #TempVentas
+	SET producto = REPLACE(producto, 'ÃƒÂº', 'ú')
+	WHERE producto LIKE '%ÃƒÂº%';
+
+	UPDATE #TempVentas
+	SET producto = REPLACE(producto, 'Ã±', 'ñ')
+	WHERE producto LIKE '%Ã±%';
+
+	UPDATE #TempVentas
+	SET producto = REPLACE(producto, 'Ã‘', 'Ñ')
+	WHERE producto LIKE '%Ã‘%';
+
+	UPDATE #TempVentas
+	SET producto = REPLACE(producto, 'Ã­' , 'í')
+	WHERE producto LIKE '%Ã%';
+
+	UPDATE #TempVentas
+	SET producto = REPLACE(producto, 'Ã', 'Á')
+	WHERE producto LIKE '%Ã%';
+
+	UPDATE #TempVentas
+	SET producto = REPLACE(producto, 'Âº', 'º')
+	WHERE producto LIKE '%Âº%';
+
+	BEGIN TRANSACTION
+		BEGIN TRY
+		INSERT INTO gestion_venta.Factura(id_factura,id_tipoFactura,fecha,hora,id_medioDePago,id_sucursal)
+		SELECT
+			te.id_factura,
+			tf.id id_tipoFactura,
+			TRY_CONVERT(DATE, te.fecha, 101) AS fecha,
+			CONVERT(TIME(7), te.hora) AS hora_convertida,
+			mp.id id_medioDePago,
+			s.id
+		FROM #TempVentas te 
+		JOIN gestion_venta.TipoFactura tf ON tf.nombre = te.tipo_factura
+		JOIN gestion_venta.MedioDePago mp ON mp.nombre = te.medio_pago 
+		JOIN gestion_sucursal.Sucursal s ON ( 
+				(s.nombre = 'San Justo' AND te.ciudad = 'Yangon') OR 
+				(s.nombre = 'Ramos mejia' AND te.ciudad = 'Naypyitaw') OR 
+				(s.nombre = 'Lomas del mirador' AND te.ciudad = 'Mandalay') OR 
+				s.nombre = te.ciudad
+			)
+		WHERE NOT EXISTS (
+		SELECT 1
+		FROM gestion_venta.Factura f
+		WHERE f.id_factura = te.id_factura
+		);
+
+		INSERT INTO gestion_venta.DetalleVenta(id_factura, cantidad, precio_unitario, id_producto, subtotal)
+		SELECT 
+			te.id_factura, 
+			te.cantidad, 
+			precio_unitario, 
+			producto.id id_producto,
+			(precio_unitario * cantidad) AS subtotal
+		FROM 
+			#TempVentas te 
+		JOIN 
+			gestion_producto.Producto producto 
+		ON 
+			producto.descripcion = te.producto
+		WHERE NOT EXISTS (
+		SELECT 1
+		FROM gestion_venta.DetalleVenta dv
+		WHERE dv.id_factura = te.id_factura
+		  AND dv.id_producto = producto.id
+		);
+		COMMIT TRANSACTION;
+		END TRY
+	BEGIN CATCH
+		PRINT ERROR_MESSAGE(); 
+		ROLLBACK TRANSACTION;
+	END CATCH
+
+	DROP TABLE #TempVentas;
 END
 GO
 
@@ -73,8 +157,12 @@ EXEC Importar_ventas_csv @RutaArchivo
 
 
 
---select * from ##TempVentas;
-select * from gestion_venta.DetalleVenta
---DELETE FROM gestion_venta.DetalleVenta
+/*SELECT * from #TempVentas;
+SELECT dv.id,p.descripcion
+FROM gestion_venta.DetalleVenta dv INNER JOIN gestion_producto.Producto p ON p.id = dv.id_producto 
+ORDER BY id_factura
+DELETE FROM gestion_venta.DetalleVenta
 select * from gestion_venta.Factura
---DELETE FROM gestion_venta.Factura
+DELETE FROM gestion_venta.Factura
+DBCC CHECKIDENT ('gestion_venta.Factura', RESEED, 0);
+DBCC CHECKIDENT ('gestion_venta.DetalleVenta', RESEED, 0);*/
