308a3cea7cc0f1ca88e7dbd75f2547ba6ed0f29e
Abigail Karina <89662554+AbigailKarina@users.noreply.github.com>
2024-11-14 15:50:56 -0300

    Update 04_SP de Borrado.sql
    Cambiados print por Raiserror y agregados Borrar_Proveedor y Borrar_NotaDeCredito

--- SQL/TP_Integrador/Com5600G05/04_SP de Borrado.sql
+++ SQL/TP_Integrador/Com5600G05/04_SP de Borrado.sql
@@ -4,9 +4,10 @@ GO
 -- ============================ SP BORRADO GESTION_SUCURSAL ============================
 
 CREATE OR ALTER PROCEDURE gestion_sucursal.Borrar_Sucursal
-    @sucursalID INT
+	@sucursalID INT
 AS
 BEGIN
+	-- Si la sucursal existe y esta activa
 	IF EXISTS (SELECT 1 FROM gestion_sucursal.Sucursal WHERE id = @sucursalID AND activo = 1)
 	BEGIN
 		UPDATE gestion_sucursal.Empleado
@@ -14,163 +15,228 @@ BEGIN
 		WHERE id_sucursal = @sucursalID
 
 		UPDATE gestion_sucursal.Sucursal
-		SET activo = 0 WHERE id = @sucursalID
-		PRINT 'La sucursal ' + CAST(@sucursalID AS VARCHAR) + ' fue dada de baja.'
+		SET activo = 0
+		WHERE id = @sucursalID
+		
+		PRINT 'La sucursal con ID ' + CAST(@sucursalID AS VARCHAR) + ' fue dada de baja correctamente.';
 	END
 	ELSE
 	BEGIN
-		PRINT 'La sucursal no existe o ya fue dada de baja.';
-	END		
+		RAISERROR('La sucursal con ID %d no existe o ya fue dada de baja.', 16, 1, @sucursaID);
+	END
+END
+GO
+
+CREATE OR ALTER PROCEDURE gestion_sucursal.Borrar_Turno
+	@turnoID INT
+AS
+BEGIN
+	-- Si el turno existe y esta activo
+	IF EXISTS (SELECT 1 FROM gestion_sucursal.Turno WHERE id = @turnoID AND activo = 1)
+	BEGIN
+		UPDATE gestion_sucursal.Turno
+		SET activo = 0
+		WHERE id = @turnoID;
+
+		PRINT 'El turno con ID ' + CAST(@turnoID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El turno con ID %d no existe o ya fue dado de baja.', 16, 1, @turnoID);
+	END
+END
+GO
+
+CREATE OR ALTER PROCEDURE gestion_sucursal.Borrar_Cargo
+	@cargoID INT
+AS
+BEGIN
+	-- Si el cargo existe y esta activo
+	IF EXISTS (SELECT 1 FROM gestion_sucursal.Cargo WHERE id = @cargoID AND activo = 1)
+	BEGIN
+		UPDATE gestion_sucursal.Cargo
+		SET activo = 0
+		WHERE id = @cargoID;
+
+		PRINT 'El cargo con ID ' + CAST(@cargoID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El cargo con ID %d no existe o ya fue dado de baja.', 16, 1, @cargoID);
+	END
 END
 GO
 
 CREATE OR ALTER PROCEDURE gestion_sucursal.Borrar_Empleado
-    @empleadoID INT
+	@empleadoID INT
 AS
 BEGIN
-    IF EXISTS (SELECT 1 FROM gestion_sucursal.Empleado WHERE id = @empleadoID AND activo = 1)
-    BEGIN
-        UPDATE gestion_sucursal.Empleado
-        SET activo = 0
-        WHERE id = @empleadoID;
+	-- Si el empleado existe y esta activo
+	IF EXISTS (SELECT 1 FROM gestion_sucursal.Empleado WHERE id = @empleadoID AND activo = 1)
+	BEGIN
+		UPDATE gestion_sucursal.Empleado
+		SET activo = 0
+		WHERE id = @empleadoID;
 
-        PRINT 'Empleado borrado correctamente.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'El empleado no existe o ya fue dado de baja.';
+		PRINT 'El empleado con ID ' + CAST(@empleadoID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El empleado con ID %d no existe o ya fue dado de baja.', 16, 1, @empleadoID);
     END
 END
 GO
 
 CREATE OR ALTER PROCEDURE gestion_sucursal.Borrar_TipoCliente
-    @tipoClienteID INT
+	@tipoClienteID INT
 AS
 BEGIN
     -- Si el tipo de cliente existe y esta activo
-    IF EXISTS (SELECT 1 FROM gestion_sucursal.TipoCliente WHERE id = @tipoClienteID AND activo = 1)
-    BEGIN
-        UPDATE gestion_sucursal.TipoCliente
-        SET activo = 0
-        WHERE id = @tipoClienteID;
+	IF EXISTS (SELECT 1 FROM gestion_sucursal.TipoCliente WHERE id = @tipoClienteID AND activo = 1)
+	BEGIN
+		UPDATE gestion_sucursal.TipoCliente
+		SET activo = 0
+		WHERE id = @tipoClienteID;
 
-        PRINT 'Tipo de cliente borrado correctamente.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'El tipo de cliente no existe o ya fue dado de baja.';
-    END
+		PRINT 'El tipo de cliente con ID ' + CAST(@tipoClienteID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El tipo de cliente con ID %d no existe o ya fue dado de baja.', 16, 1, @tipoClienteID);
+	END
 END
 GO
 
 CREATE OR ALTER PROCEDURE gestion_sucursal.Borrar_Genero
-    @generoID INT
+	@generoID INT
 AS
 BEGIN
-    -- Si el genero existe y esta activo
-    IF EXISTS (SELECT 1 FROM gestion_sucursal.Genero WHERE id = @generoID AND activo = 1)
-    BEGIN
-        UPDATE gestion_sucursal.Genero
-        SET activo = 0
-        WHERE id = @generoID;
+	-- Si el genero existe y esta activo
+	IF EXISTS (SELECT 1 FROM gestion_sucursal.Genero WHERE id = @generoID AND activo = 1)
+	BEGIN
+		UPDATE gestion_sucursal.Genero
+		SET activo = 0
+		WHERE id = @generoID;
 
-        PRINT 'Genero borrado correctamente.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'El genero no existe o ya fue dado de baja.';
-    END
+		PRINT 'El genero con ID ' + CAST(@generoID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El genero con ID %d no existe o ya fue dado de baja.', 16, 1, @generoID);
+	END
 END
 GO
 
 CREATE OR ALTER PROCEDURE gestion_sucursal.Borrar_Cliente
-    @clienteID INT
+	@clienteID INT
 AS
 BEGIN
     -- Si el cliente existe y esta activo
-    IF EXISTS (SELECT 1FROM gestion_sucursal.Cliente WHERE id = @clienteID AND activo = 1)
-    BEGIN
-        UPDATE gestion_sucursal.Cliente
-        SET activo = 0
-        WHERE id = @clienteID;
+	IF EXISTS (SELECT 1FROM gestion_sucursal.Cliente WHERE id = @clienteID AND activo = 1)
+	BEGIN
+		UPDATE gestion_sucursal.Cliente
+		SET activo = 0
+		WHERE id = @clienteID;
 
-        PRINT 'Cliente borrado correctamente.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'El cliente no existe o ya fue dado de baja.';
-    END
+		PRINT 'El cliente con ID ' + CAST(@clienteID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El cliente con ID %d no existe o ya fue dado de baja.', 16, 1, @clienteID);
+	END
 END
 GO
 
 -- ============================ SP BORRADO GESTION_PRODUCTO ============================
 
-CREATE OR ALTER PROCEDURE gestion_producto.Borrar_TipoProducto
-    @tipoProductoID INT
+CREATE OR ALTER PROCEDURE gestion_producto.Borrar_Proveedor
+	@proveedorID INT
 AS
 BEGIN
-    -- Si el tipo de producto existe y esta activo
-    IF EXISTS (SELECT 1 FROM gestion_producto.TipoProducto WHERE id = @tipoProductoID AND activo = 1)
-    BEGIN
-	-- Inactivo a todas la categorias del tipoProductoID
+	-- Si el proveedor existe y esta activo
+	IF EXISTS (SELECT 1 FROM gestion_producto.Proveedor WHERE id = @proveedorID AND activo = 1)
+	BEGIN
+		UPDATE gestion_producto.Proveedor
+		SET activo = 0
+		WHERE id = @proveedorID;
+
+		PRINT 'El proveedor con ID ' + CAST(@proveedorID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El proveedor con ID %d no existe o ya fue dado de baja.', 16, 1, @proveedorID);
+	END
+END
+GO
+
+CREATE OR ALTER PROCEDURE gestion_producto.Borrar_TipoProducto
+	@tipoProductoID INT
+AS
+BEGIN
+	-- Si el tipo de producto existe y esta activo
+	IF EXISTS (SELECT 1 FROM gestion_producto.TipoProducto WHERE id = @tipoProductoID AND activo = 1)
+	BEGIN
+	-- Inactivo a todas las categorias del tipoProductoID
 		UPDATE gestion_producto.Categoria
-        SET activo = 0
-        WHERE id_tipoProducto = @tipoProductoID;
+		SET activo = 0
+		WHERE id_tipoProducto = @tipoProductoID;
 
-        UPDATE gestion_producto.TipoProducto
-        SET activo = 0
-        WHERE id = @tipoProductoID;
+	-- Inactivo al tipoProductoID
+		UPDATE gestion_producto.TipoProducto
+		SET activo = 0
+		WHERE id = @tipoProductoID;
 
-        PRINT 'Tipo de producto borrado correctamente.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'El tipo de producto no existe o ya esta dado de baja.';
-    END
+		PRINT 'El tipo de producto con ID ' + CAST(@tipoProductoID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El tipo de producto con ID %d no existe o ya fue dado de baja.', 16, 1, @tipoProductoID);
+	END
 END
 GO
 
 CREATE OR ALTER PROCEDURE gestion_producto.Borrar_Categoria
-    @categoriaID INT
+	@categoriaID INT
 AS
 BEGIN
     -- Si la categoria existe y esta activa
-    IF EXISTS (SELECT 1 FROM gestion_producto.Categoria WHERE id = @categoriaID AND activo = 1)
-    BEGIN
+	IF EXISTS (SELECT 1 FROM gestion_producto.Categoria WHERE id = @categoriaID AND activo = 1)
+	BEGIN
 	-- Inactivo a todos los productos de categoriaID
 		UPDATE gestion_producto.Producto
-        SET activo = 0
-        WHERE id_categoria = @categoriaID;
+		SET activo = 0
+		WHERE id_categoria = @categoriaID;
 
-        UPDATE gestion_producto.Categoria
-        SET activo = 0
-        WHERE id = @categoriaID;
+	-- Inactivo la categoriaID
+		UPDATE gestion_producto.Categoria
+		SET activo = 0
+		WHERE id = @categoriaID;
 
-        PRINT 'Categoria borrada correctamente.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'La categoria no existe o ya esta dada de baja.';
+		PRINT 'La categoria con ID ' + CAST(@categoriaID AS VARCHAR) + ' fue dada de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('La categoria con ID %d no existe o ya fue dada de baja.', 16, 1, @categoriaID);
     END
 END
 GO
 
 CREATE OR ALTER PROCEDURE gestion_producto.Borrar_Producto
-    @productoID INT
+	@productoID INT
 AS
 BEGIN
-    -- Si el producto existe y esta activo
-    IF EXISTS (SELECT 1 FROM gestion_producto.Producto WHERE id = @productoID AND activo = 1)
-    BEGIN
-        UPDATE gestion_producto.Producto
-        SET activo = 0
-        WHERE id = @productoID;
+	-- Si el producto existe y esta activo
+	IF EXISTS (SELECT 1 FROM gestion_producto.Producto WHERE id = @productoID AND activo = 1)
+	BEGIN
+		UPDATE gestion_producto.Producto
+		SET activo = 0
+		WHERE id = @productoID;
 
-        PRINT 'Producto borrado correctamente.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'El producto no existe o ya fue eliminado.';
+		PRINT 'El producto con ID ' + CAST(@productoID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El producto con ID %d no existe o ya fue dado de baja.', 16, 1, @productoID);
     END
 END
 GO
@@ -178,87 +244,111 @@ GO
 -- ============================ SP BORRADO GESTION_VENTA ============================
 
 CREATE OR ALTER PROCEDURE gestion_venta.Borrar_MedioDePago
-    @medioDePagoID INT
+	@medioDePagoID INT
 AS
 BEGIN
     -- Si el medio de pago existe y esta activo
-    IF EXISTS (SELECT 1 FROM gestion_venta.MedioDePago WHERE id = @medioDePagoID AND activo = 1)
-    BEGIN
-        UPDATE gestion_venta.MedioDePago
-        SET activo = 0
-        WHERE id = @medioDePagoID;
+	IF EXISTS (SELECT 1 FROM gestion_venta.MedioDePago WHERE id = @medioDePagoID AND activo = 1)
+	BEGIN
+		UPDATE gestion_venta.MedioDePago
+		SET activo = 0
+		WHERE id = @medioDePagoID;
 
-        PRINT 'Medio de pago borrado correctamente.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'El medio de pago no existe o ya esta inactivo.';
-    END
+		PRINT 'El medio de pago con ID ' + CAST(@medioDePagoID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El medio de pago con ID %d no existe o ya fue dado de baja.', 16, 1, @medioDePagoID);
+	END
 END
 GO
 
 CREATE OR ALTER PROCEDURE gestion_venta.Borrar_TipoFactura
-    @tipoFacturaID INT
+	@tipoFacturaID INT
 AS
 BEGIN
-    -- Si el tipo de factura existe y esta activo
-    IF EXISTS (SELECT 1 FROM gestion_venta.TipoFactura WHERE id = @tipoFacturaID AND activo = 1)
-    BEGIN
-        UPDATE gestion_venta.TipoFactura
-        SET activo = 0
-        WHERE id = @tipoFacturaID;
+	-- Si el tipo de factura existe y esta activo
+	IF EXISTS (SELECT 1 FROM gestion_venta.TipoFactura WHERE id = @tipoFacturaID AND activo = 1)
+	BEGIN
+		UPDATE gestion_venta.TipoFactura
+		SET activo = 0
+		WHERE id = @tipoFacturaID;
 
-        PRINT 'Tipo de factura borrado correctamente.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'El tipo de factura no existe o ya esta inactivo.';
-    END
+		PRINT 'El tipo de factura con ID ' + CAST(@tipoFacturaID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El tipo de factura con ID %d no existe o ya fue dado de baja.', 16, 1, @tipoFacturaID);
+	END
 END
 GO
 
 CREATE OR ALTER PROCEDURE gestion_venta.Borrar_Factura
-    @facturaID CHAR(11)
+	@facturaID		CHAR(11)
 AS
 BEGIN
-    -- Si la factura existe y esta activa
-    IF EXISTS (SELECT 1 FROM gestion_venta.Factura WHERE id_factura = @facturaID AND activo = 1)
-    BEGIN
+	-- Si la factura existe y esta activa
+	IF EXISTS (SELECT 1 FROM gestion_venta.Factura WHERE id_factura = @facturaID AND activo = 1)
+	BEGIN
 	-- Inactivo todos los detalles de venta de facturaID
 		UPDATE gestion_venta.DetalleVenta
 		SET activo = 0 
 		WHERE id_factura = @facturaID;
 
-        UPDATE gestion_venta.Factura
-        SET activo = 0
-        WHERE id_factura = @facturaID;
+	-- Inactivo la facturaID
+		UPDATE gestion_venta.Factura
+		SET activo = 0
+		WHERE id_factura = @facturaID;
 
-        PRINT 'Factura borrada correctamente.';
-    END
-    ELSE
+		PRINT 'La factura con ID ' + CAST(@facturaID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
 	BEGIN
-        PRINT 'La factura no existe.';
+		RAISERROR('La factura con ID %d no existe o ya fue dada de baja.', 16, 1, @facturaID);
 	END
 END
 GO
 
 CREATE OR ALTER PROCEDURE gestion_venta.Borrar_DetalleVenta
-    @detalleVentaID INT,
-    @facturaID CHAR(11)
+	@detalleVentaID		INT,
+	@facturaID			CHAR(11)
 AS
 BEGIN
-    IF EXISTS (	SELECT 1 FROM gestion_venta.DetalleVenta
+	-- Si existe el detalle de venta para esa factura y esta activo
+	IF EXISTS (	SELECT 1 FROM gestion_venta.DetalleVenta
 				WHERE id = @detalleVentaID AND id_factura = @facturaID AND activo = 1 )
-    BEGIN
-        UPDATE gestion_venta.DetalleVenta
-        SET activo = 0
-        WHERE id = @detalleVentaID AND id_factura = @facturaID;
-
-        PRINT 'Detalle de venta borrado correctamente.';
-    END
-    ELSE
 	BEGIN
-        PRINT 'El detalle de venta no existe o ya fue eliminado.';
+		UPDATE gestion_venta.DetalleVenta
+		SET activo = 0
+		WHERE id = @detalleVentaID AND id_factura = @facturaID;
+
+		PRINT 'El detalle de venta con ID ' + CAST(@tipoFacturaID AS VARCHAR) +
+			' para la factura con ID ' + CAST(@facturaID AS VARCHAR) + ' fue dado de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('El detalle de venta con ID %d no existe o ya fue dado de baja.', 16, 1, @tipoFacturaID);
+	END
+END
+GO
+
+CREATE OR ALTER PROCEDURE gestion_venta.Borrar_NotaDeCredito
+	@notaCreditoID INT
+AS
+BEGIN
+    -- Si la nota de credito existe y esta activa
+	IF EXISTS (SELECT 1 FROM gestion_venta.NotaDeCredito WHERE id = @notaCreditoID AND activo = 1)
+	BEGIN
+		UPDATE gestion_venta.NotaDeCredito
+		SET activo = 0
+		WHERE id = @notaCreditoID;
+
+		PRINT 'La nota de crédito con ID ' + CAST(@notaCreditoID AS VARCHAR) +
+			' para la factura con ID ' + CAST(@facturaID AS VARCHAR) + ' fue dada de baja correctamente.';
+	END
+	ELSE
+	BEGIN
+		RAISERROR('La nota de crédito con ID %d no existe o ya fue dada de baja.', 16, 1, @notaCreditoID);
 	END
 END
 GO
