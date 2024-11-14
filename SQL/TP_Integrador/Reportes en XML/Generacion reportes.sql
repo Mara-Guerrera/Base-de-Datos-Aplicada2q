57bf6f434196e7bc493f816ed5d61bcc2207b973
Pilubou <labrujacosmica1970@gmail.com>
2024-11-14 19:40:09 -0300

    Update Generacion reportes.sql
    Faltan los tres últimos reportes que dicen Mostrar.

--- SQL/TP_Integrador/Reportes en XML/Generacion reportes.sql
+++ SQL/TP_Integrador/Reportes en XML/Generacion reportes.sql
@@ -1,16 +1,24 @@
 /*El sistema debe ofrecer los siguientes reportes en xml.
-Mensual: ingresando un mes y aÒo determinado mostrar el total facturado por dÌas de
-la semana, incluyendo s·bado y domingo.
+Mensual: ingresando un mes y a√±o determinado mostrar el total facturado por d√≠as de
+la semana, incluyendo s√°bado y domingo.
 Trimestral: mostrar el total facturado por turnos de trabajo por mes.
 Por rango de fechas: ingresando un rango de fechas a demanda, debe poder mostrar
 la cantidad de productos vendidos en ese rango, ordenado de mayor a menor.
 Por rango de fechas: ingresando un rango de fechas a demanda, debe poder mostrar
 la cantidad de productos vendidos en ese rango por sucursal, ordenado de mayor a
 menor.
-Mostrar los 5 productos m·s vendidos en un mes, por semana
+Mostrar los 5 productos m√°s vendidos en un mes, por semana
 Mostrar los 5 productos menos vendidos en el mes.
 Mostrar total acumulado de ventas (o sea tambien mostrar el detalle) para una fecha
-y sucursal particulares*/DECLARE @anio INT DECLARE @mes INTSET @anio = 2019SET @mes = 2;WITH Consulta AS( SELECT 
+y sucursal particulares*/
+
+DECLARE @anio INT 
+DECLARE @mes INT
+SET @anio = 2019
+SET @mes = 2;
+
+WITH Consulta AS
+( SELECT 
         DATENAME(weekday, f.fecha) AS DiaSemana, 
         dv.subtotal
   FROM 
@@ -19,22 +27,26 @@ y sucursal particulares*/DECLARE @anio INT DECLARE @mes INTSET @anio = 2019
        gestion_venta.Factura f ON dv.id_factura = f.id
   WHERE 
        YEAR(f.fecha) = @anio 
-       AND MONTH(f.fecha) = @mes)SELECT 
-    @anio AS AÒo,
+       AND MONTH(f.fecha) = @mes
+)
+SELECT 
+    @anio AS A√±o,
     @mes AS Mes,
     [lunes] AS Lunes,
     [martes] AS Martes,
-    [miÈrcoles] AS MiÈrcoles,
+    [mi√©rcoles] AS Mi√©rcoles,
     [jueves] AS Jueves,
     [viernes] AS Viernes,
-    [s·bado] AS S·bado,
+    [s√°bado] AS S√°bado,
     [domingo] AS Domingo
 FROM 
     Consulta
 PIVOT (
     SUM(subtotal)
-    FOR DiaSemana IN ([lunes], [martes], [miÈrcoles], [jueves], [viernes], [s·bado], [domingo])
-) AS PFOR XML PATH('DÌas'), ROOT('Reporte');WITH subquery AS (
+    FOR DiaSemana IN ([lunes], [martes], [mi√©rcoles], [jueves], [viernes], [s√°bado], [domingo])
+) AS P
+FOR XML PATH('D√≠as'), ROOT('Reporte');
+WITH subquery AS (
     SELECT 
         dv.subtotal,
         e.id_turno,
@@ -74,3 +86,53 @@ INNER JOIN
 ORDER BY 
     t.descripcion
 FOR XML PATH('Turno'), ROOT('Reporte');
+
+
+--Rango de fechas, mostrar cantidad de productos vendidos de menor a mayor--
+DECLARE @fecha_1 DATE
+DECLARE @fecha_2 DATE
+SET @fecha_1 = '2019-01-01'
+SET @fecha_2 = '2019-02-04'
+
+SELECT 
+    dv.id_producto,
+    p.descripcion,
+    SUM(dv.cantidad) AS total_vendido
+FROM 
+    gestion_venta.DetalleVenta dv
+INNER JOIN 
+    gestion_venta.Factura f ON dv.id_factura = f.id
+INNER JOIN 
+    gestion_producto.Producto p ON dv.id_producto = p.id
+WHERE 
+    f.fecha BETWEEN @fecha_1 AND @fecha_2
+GROUP BY 
+    dv.id_producto, 
+    p.descripcion
+ORDER BY 
+    total_vendido DESC
+FOR XML PATH('Productos'), ROOT('ReporteDeVentas');
+--Rango de fechas: mostrar cantidad de productos vendidos de menor a mayor, por sucursal--
+DECLARE @fecha_inicio DATE
+DECLARE @fecha_fin DATE
+SET @fecha_inicio = '2019-01-01'
+SET @fecha_fin = '2019-06-04'
+
+SELECT 
+    DISTINCT dv.id_producto,
+    p.descripcion AS descripcion_producto,
+    s.nombre AS nombre_sucursal,
+	SUM(dv.cantidad) OVER (PARTITION BY f.id_sucursal,dv.id_producto) AS cant_prod_sucursal
+FROM 
+    gestion_venta.Factura f
+INNER JOIN 
+    gestion_venta.DetalleVenta dv ON f.id = dv.id_factura
+INNER JOIN 
+    gestion_producto.Producto p ON dv.id_producto = p.id
+INNER JOIN 
+    gestion_sucursal.Sucursal s ON s.id = f.id_sucursal
+WHERE 
+    f.fecha BETWEEN @fecha_inicio AND @fecha_fin
+ORDER BY 
+    s.nombre, cant_prod_sucursal
+FOR XML PATH('ProductoPorSucursal'), ROOT('Resultado')
