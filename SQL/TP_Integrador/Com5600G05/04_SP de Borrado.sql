e9ad41eb32fafb8ab61595057a5a2470d47b2f99
Abigail Karina <89662554+AbigailKarina@users.noreply.github.com>
2024-11-15 15:37:36 -0300

    Update 04_SP de Borrado.sql
    Movido Borrar_NotaDeCredito al scrip de Nota de credito y Devolucion

--- SQL/TP_Integrador/Com5600G05/04_SP de Borrado.sql
+++ SQL/TP_Integrador/Com5600G05/04_SP de Borrado.sql
@@ -376,34 +376,3 @@ BEGIN
 	END
 END
 GO
-
-IF OBJECT_ID('[gestion_venta].[Borrar_NotaDeCredito]', 'P') IS NOT NULL
-    DROP PROCEDURE [gestion_venta].[Borrar_NotaDeCredito];
-GO
-CREATE PROCEDURE gestion_venta.Borrar_NotaDeCredito
-	@notaCreditoID INT
-AS
-BEGIN
-    -- Si la nota de credito existe y esta activa
-	IF EXISTS (SELECT 1 FROM gestion_venta.NotaCredito WHERE id = @notaCreditoID AND activo = 1)
-	BEGIN
-		DECLARE @facturaID CHAR(11)
-
-		SELECT @facturaID = f.id_factura
-		FROM gestion_venta.Factura f 
-		INNER JOIN gestion_venta.NotaCredito nc ON f.id = nc.id_factura
-		WHERE nc.id = @notaCreditoID
-
-		UPDATE gestion_venta.NotaDeCredito
-		SET activo = 0
-		WHERE id = @notaCreditoID;
-
-		PRINT 'La nota de crédito con ID ' + CAST(@notaCreditoID AS VARCHAR) +
-			' para la factura con ID ' + ISNULL(@facturaID, 'desconocido') + ' fue dada de baja correctamente.';
-	END
-	ELSE
-	BEGIN
-		RAISERROR('La nota de crédito con ID %d no existe o ya fue dada de baja.', 16, 1, @notaCreditoID);
-	END
-END
-GO
