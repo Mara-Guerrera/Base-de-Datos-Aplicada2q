bb7f13a31c7d6fd73a9a1c33290f7528197de1f2
Pilubou <labrujacosmica1970@gmail.com>
2024-11-15 13:00:36 -0300

    Update 08_PruebaNota.sql

--- SQL/TP_Integrador/Testing/08_PruebaNota.sql
+++ SQL/TP_Integrador/Testing/08_PruebaNota.sql
@@ -1,22 +1,44 @@
-/*Cuando un cliente reclama la devoluciÛn de un producto se genera una nota de crÈdito por el
+/*Cuando un cliente reclama la devoluci√≥n de un producto se genera una nota de cr√©dito por el
 valor del producto o un producto del mismo tipo.
-En el caso de que el cliente solicite la nota de crÈdito, solo los Supervisores tienen el permiso
+En el caso de que el cliente solicite la nota de cr√©dito, solo los Supervisores tienen el permiso
 para generarla.
-Tener en cuenta que la nota de crÈdito debe estar asociada a una Factura con estado pagada.
+Tener en cuenta que la nota de cr√©dito debe estar asociada a una Factura con estado pagada.
 Asigne los roles correspondientes para poder cumplir con este requisito.
 Por otra parte, se requiere que los datos de los empleados se encuentren encriptados, dado
-que los mismos contienen informaciÛn personal.*/
+que los mismos contienen informaci√≥n personal.*/
 
 USE Com5600G05
-SELECT * FROM gestion_venta.DetalleVenta
-SELECT * FROM gestion_venta.Factura
+--Muestro el rol actual--
+SELECT 
+    dp.name AS RoleName
+FROM 
+    sys.database_role_members drm
+JOIN 
+    sys.database_principals dp 
+    ON drm.role_principal_id = dp.principal_id
+WHERE 
+    drm.member_principal_id = USER_ID();
 
+SELECT dp.name AS Supervisor, 
+       dp2.name AS Grupo5
+FROM   sys.database_role_members drm
+       JOIN sys.database_principals dp 
+       ON drm.role_principal_id = dp.principal_id
+       JOIN sys.database_principals dp2
+       ON drm.member_principal_id = dp2.principal_id
+WHERE  dp2.name = 'Grupo5';
+--Invocaci√≥n a SP de generar nota de cr√©dito--
+DECLARE @precio_producto DECIMAL(7,2)
+SELECT @precio_producto = precio
+FROM gestion_producto.Producto WHERE id = 3419 
+EXEC gestion_venta.GenerarNotaCredito 10,3419,@precio_producto,1
+
+SELECT @precio_producto = precio
+FROM gestion_producto.Producto WHERE id = 2713 
+EXEC gestion_venta.GenerarNotaCredito 12,2713,@precio_producto,1
 
 
 /*
-SELECT * FROM gestion_venta.Factura
-SELECT * FROM gestion_venta.DetalleVenta
-
-SELECT c.id, nombre, apellido, g.descripcion, tc.descripcion
-FROM gestion_sucursal.Cliente c INNER JOIN gestion_sucursal.Genero g ON c.id_genero = g.id
-INNER JOIN gestion_sucursal.TipoCliente tc ON c.id_tipo = tc.id 
+SELECT * FROM gestion_venta.DetalleVenta 
+SELECT * FROM gestion_venta.NotaCredito 
+*/ 
