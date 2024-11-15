56e1389cecdaafbf52935ba8493edabdc760f452
Pilubou <labrujacosmica1970@gmail.com>
2024-11-14 23:40:31 -0300

    Update 08_NotasCreditoYDevoluciones.sql

--- SQL/TP_Integrador/Seguridad/08_NotasCreditoYDevoluciones.sql
+++ SQL/TP_Integrador/Seguridad/08_NotasCreditoYDevoluciones.sql
@@ -1,16 +1,28 @@
 -----ROL SUPERVISOR-----
+USE Com5600G05
 -- Crear un nuevo usuario de base de datos
 IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'Supervisor')
 BEGIN
     CREATE ROLE Supervisor;
 END
 
-CREATE LOGIN Grupo5 WITH PASSWORD = 'Grupo5';
+IF NOT EXISTS (SELECT 1 FROM sys.server_principals WHERE name = 'Grupo5')
+BEGIN
+    CREATE LOGIN Grupo5 WITH PASSWORD = 'Grupo5';
+END
 
-CREATE USER Grupo5 FOR LOGIN Grupo5;
+IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'Grupo5')
+BEGIN
+    CREATE USER Grupo5 FOR LOGIN Grupo5;
+END
 
--- Asignar el rol Supervisor al usuario creado
-EXEC sp_addrolemember 'Supervisor', 'Grupo5';
+--Asignar el rol Supervisor al usuario creado solo si no tiene el rol
+IF NOT EXISTS (SELECT 1 FROM sys.database_role_members 
+               WHERE role_principal_id = DATABASE_PRINCIPAL_ID('Supervisor') 
+               AND member_principal_id = USER_ID('Grupo5'))
+BEGIN
+    EXEC sp_addrolemember 'Supervisor', 'Grupo5';
+END
 
 -----NOTA DE CREDITO----
 IF NOT EXISTS (
@@ -33,13 +45,10 @@ BEGIN
         activo              BIT DEFAULT 1,
 
         CONSTRAINT PK_NotaCreditoID PRIMARY KEY (id),
-        CONSTRAINT FK_FacturaID FOREIGN KEY (id_factura) REFERENCES gestion_venta.Factura(id),
-        CONSTRAINT FK_ProductoID FOREIGN KEY (id_producto) REFERENCES gestion_producto.Producto(id),
+        CONSTRAINT FK_FacturaID4 FOREIGN KEY (id_factura) REFERENCES gestion_venta.Factura(id),
+        CONSTRAINT FK_ProductoID4 FOREIGN KEY (id_producto) REFERENCES gestion_producto.Producto(id),
         CONSTRAINT FK_SupervisorID FOREIGN KEY (id_supervisor) REFERENCES gestion_sucursal.Empleado(id),
-        CONSTRAINT CK_ValorCredito CHECK (valor_credito > 0),
-        CONSTRAINT CK_FacturaPagada CHECK (
-            EXISTS (SELECT 1 FROM gestion_venta.Factura WHERE id = id_factura AND pagada = 1)
-        )
+        CONSTRAINT CK_ValorCredito CHECK (valor_credito > 0)
     )
 END
 GO
@@ -47,7 +56,11 @@ GO
 -- Otorgar permisos al rol Supervisor
 GRANT INSERT, SELECT ON gestion_venta.NotaCredito TO Supervisor;
 
-CREATE OR ALTER PROCEDURE gestion_venta.GenerarNotaCredito
+IF OBJECT_ID('[gestion_venta].[GenerarNotaCredito]', 'P') IS NOT NULL
+    DROP PROCEDURE [gestion_venta].[GenerarNotaCredito];
+
+GO
+CREATE PROCEDURE gestion_venta.GenerarNotaCredito
     @id_factura INT,
     @id_producto INT,
     @valor_credito DECIMAL(7,2),
@@ -97,10 +110,7 @@ BEGIN
         CONSTRAINT FK_ProductoIDDevolucion FOREIGN KEY (id_producto) REFERENCES gestion_producto.Producto(id),
         CONSTRAINT FK_SupervisorIDDevolucion FOREIGN KEY (id_supervisor) REFERENCES gestion_sucursal.Empleado(id),
         CONSTRAINT FK_NotaCreditoIDDevolucion FOREIGN KEY (id_notaCredito) REFERENCES gestion_venta.NotaCredito(id),
-        CONSTRAINT CK_CantidadDevolucion CHECK (cantidad > 0),
-        CONSTRAINT CK_FacturaPagadaDevolucion CHECK (
-            EXISTS (SELECT 1 FROM gestion_venta.Factura WHERE id = id_factura AND pagada = 1)
-        )
+        CONSTRAINT CK_CantidadDevolucion CHECK (cantidad > 0)
     )
 END
 GO
@@ -108,12 +118,15 @@ GO
 -- Otorgar permisos al rol Supervisor
 GRANT INSERT, SELECT, UPDATE ON gestion_venta.Devolucion TO Supervisor;
 
+IF OBJECT_ID('[gestion_venta].[RegistrarDevolucion]', 'P') IS NOT NULL
+    DROP PROCEDURE [gestion_venta].[RegistrarDevolucion];
+GO
 CREATE PROCEDURE gestion_venta.RegistrarDevolucion
     @id_factura INT,
     @id_producto INT,
     @cantidad INT,
     @motivo VARCHAR(255),
-    @id_supervisor INT,
+    @id_supervisor INT
 AS
 BEGIN
     -- Validar que la factura esté pagada
