a016315624ce51c2547b67ce47d6e0fd179ad5ad
Abigail Karina <89662554+AbigailKarina@users.noreply.github.com>
2024-11-24 02:18:07 -0300

    Update and rename 05_Invocacion SP Importaciones.sql to 09_InvocacionSPImportaciones.sql
    CAMBIOS POS ENTREGA TnT: Añadidos los GO y título resaltado

---
+++ SQL/TP_Integrador/Testing/09_InvocacionSPImportaciones.sql
@@ -0,0 +1,34 @@
+USE Com5600G05
+DECLARE @RutaArchivo NVARCHAR(255);
+
+-- ============================ Inserción de datos de la empresa ============================
+
+DECLARE @telefono VARCHAR(20);
+SET @telefono = '11 6846-2003'; 
+INSERT INTO gestion_sucursal.Empresa(cuit,razon_social,telefono) VALUES ('20-12801863-9','Aurora S.A.',@telefono)
+GO
+
+SET @RutaArchivo = N'C:\Users\Public\Downloads\TP_integrador_Archivos\Informacion_complementaria.xlsx'
+EXEC gestion_sucursal.Importar_Sucursales @RutaArchivo
+GO
+SET @RutaArchivo = N'C:\Users\Public\Downloads\TP_integrador_Archivos\Informacion_complementaria.xlsx'
+EXEC gestion_sucursal.Importar_Empleados @RutaArchivo 
+GO
+SET @RutaArchivo = N'C:\Users\Public\Downloads\TP_integrador_Archivos\Informacion_complementaria.xlsx';
+EXEC gestion_producto.Importar_TipoProducto_Categoria @RutaArchivo;
+GO
+SET @RutaArchivo = N'C:\Users\Public\Downloads\TP_integrador_Archivos\Informacion_complementaria.xlsx';
+EXEC gestion_venta.Importar_Medios_de_Pago @RutaArchivo;
+GO
+SET @RutaArchivo = N'C:\Users\Public\Downloads\TP_integrador_Archivos\Productos\Productos_importados.xlsx'
+EXEC gestion_producto.Importar_Productos_Importados @RutaArchivo;
+GO
+SET @RutaArchivo = N'C:\Users\Public\Downloads\TP_integrador_Archivos\Productos\catalogo.csv'
+EXEC gestion_producto.Importar_catalogo_csv @RutaArchivo
+GO
+SET @RutaArchivo = N'C:\Users\Public\Downloads\TP_integrador_Archivos\Productos\Electronic accessories.xlsx'
+EXEC gestion_producto.Importar_Electronicos @RutaArchivo
+GO
+SET @RutaArchivo = N'C:\Users\Public\Downloads\TP_integrador_Archivos\Ventas_registradas.csv'
+EXEC gestion_venta.Importar_ventas_csv @RutaArchivo
+GO
