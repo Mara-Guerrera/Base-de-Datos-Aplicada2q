dd68a4e244a6af39ddd2e12e5aa7aaebb0d52f32
Abigail Karina <89662554+AbigailKarina@users.noreply.github.com>
2024-11-24 02:07:52 -0300

    Update and rename 09_Encriptacion.sql to 16_EncriptacionDeEmpleados.sql
    Inicio por Fede, versión final por Pilu. Añadido el enunciado y título resaltado

--- SQL/TP_Integrador/Seguridad/16_EncriptacionDeEmpleados.sql
+++ SQL/TP_Integrador/Seguridad/16_EncriptacionDeEmpleados.sql
@@ -1,48 +1,65 @@
-/*Cuando un cliente reclama la devolución de un producto se genera una nota de crédito por el
+/*
+		BASE DE DATOS APLICADA
+		GRUPO: 05
+		COMISION: 02-5600
+		INTEGRANTES:
+			María del Pilar Bourdieu 45289653
+			Abigail Karina Peñafiel Huayta	41913506
+			Federico Pucci 41106855
+			Mara Verónica Guerrera 40538513
+
+		FECHA DE ENTREGA: 22/11/2024
+
+ENTREGA 5:
+
+Cuando un cliente reclama la devolución de un producto se genera una nota de crédito por el
 valor del producto o un producto del mismo tipo.
 En el caso de que el cliente solicite la nota de crédito, solo los Supervisores tienen el permiso
 para generarla.
 Tener en cuenta que la nota de crédito debe estar asociada a una Factura con estado pagada.
 Asigne los roles correspondientes para poder cumplir con este requisito.
 Por otra parte, se requiere que los datos de los empleados se encuentren encriptados, dado
-que los mismos contienen información personal.*/
-
+que los mismos contienen información personal.
+*/
+-- ============================ ENCRIPTACION DE EMPLEADOS ============================
 USE Com5600G05
+GO
 
-ALTER TABLE gestion_sucursal.Empleado
-DROP CONSTRAINT UC_Legajo_Sucursal;
+--Borramos índice que puede generar problemas para la encriptación--
+DROP INDEX UC_Legajo_Sucursal ON gestion_sucursal.Empleado;
 GO
 --Agregación de campos VARBINARY--
 ALTER TABLE gestion_sucursal.Empleado
-ALTER COLUMN legajo VARBINARY(MAX);
-
+ADD legajo_ VARBINARY(MAX);
+GO
 ALTER TABLE gestion_sucursal.Empleado
 ADD nombre_ VARBINARY(MAX);
+GO
 ALTER TABLE gestion_sucursal.Empleado
 ADD direccion_ VARBINARY(MAX);
-
+GO
 ALTER TABLE gestion_sucursal.Empleado
 ADD apellido_ VARBINARY(MAX);
-
+GO
 ALTER TABLE gestion_sucursal.Empleado
 ADD dni_ VARBINARY(MAX);
-
+GO
 ALTER TABLE gestion_sucursal.Empleado
 ADD cuil_ VARBINARY(MAX);
-
+GO
 ALTER TABLE gestion_sucursal.Empleado
 ADD email_ VARBINARY(MAX);
-
+GO
 ALTER TABLE gestion_sucursal.Empleado
 ADD email_empresa_ VARBINARY(MAX);
-
+GO
 --Definición de clave--
 DECLARE @Clave VARCHAR(40)
 SET @Clave = 'cisco_1234'
 --Encriptación de datos--
 UPDATE gestion_sucursal.Empleado
 SET 
-    legajo =  EncryptByPassPhrase(@Clave, legajo),
+    legajo_ =  EncryptByPassPhrase(@Clave, CONVERT(VARBINARY(MAX), legajo)),
     nombre_ = EncryptByPassPhrase(@Clave, nombre),
     apellido_ = EncryptByPassPhrase(@Clave, apellido),
     direccion_ = EncryptByPassPhrase(@Clave, direccion),
@@ -52,7 +69,7 @@ SET
     email_empresa_ = EncryptByPassPhrase(@Clave, email_empresa);
 --Consulta con datos desencriptados--
 SELECT 
-    CONVERT(INT, DecryptByPassPhrase(@Clave, legajo)) AS legajo_desencriptado,
+    CONVERT(INT, DecryptByPassPhrase(@Clave, legajo_)) AS legajo_desencriptado,
     CONVERT(VARCHAR(MAX), DecryptByPassPhrase(@Clave, nombre_)) AS nombre_desencriptado,
     CONVERT(VARCHAR(MAX), DecryptByPassPhrase(@Clave, apellido_)) AS apellido_desencriptado,
     CONVERT(VARCHAR(MAX), DecryptByPassPhrase(@Clave, direccion_)) AS direccion_desencriptada,
@@ -61,9 +78,11 @@ SELECT
     CONVERT(VARCHAR(MAX), DecryptByPassPhrase(@Clave, email_)) AS email_desencriptado,
     CONVERT(VARCHAR(MAX), DecryptByPassPhrase(@Clave, email_empresa_)) AS email_empresa_desencriptado
 FROM gestion_sucursal.Empleado;
+SELECT * FROM gestion_sucursal.Empleado
 --Ponemos en NULL los datos de las columnas originales--
-UPDATE gestion_sucursal.Empleado
+/*UPDATE gestion_sucursal.Empleado
 SET 
+legajo = NULL,
 nombre = NULL,
 apellido =NULL, 
 direccion=NULL,
@@ -71,3 +90,15 @@ dni=NULL,
 cuil=NULL,
 email=NULL, 
 email_empresa=NULL
+--Borrado de columnas agregadas--
+ALTER TABLE gestion_sucursal.Empleado
+DROP COLUMN legajo_,
+              nombre_,
+              direccion_,
+              apellido_,
+              dni_,
+              cuil_,
+              email_,
+              email_empresa_;
+GO*/
+
