67ad1fbcaab714969d544e89bbcbd0927a76b075
Abigail Karina <89662554+AbigailKarina@users.noreply.github.com>
2024-11-24 02:25:12 -0300

    Update and rename 07_Prueba de borrar.sql to 11_PruebaDeBorrado.sql
    Pruebas por Mara. Añadidos los títulos

--- SQL/TP_Integrador/Testing/11_PruebaDeBorrado.sql
+++ SQL/TP_Integrador/Testing/11_PruebaDeBorrado.sql
@@ -1,27 +1,26 @@
+-- ============================ LOTE DE PRUEBAS DE STORED PROCEDURES ============================
 USE Com5600G05
 GO
+-- ============================ LOTE DE BORRADO POSTERIOR A IMPORTACION ============================
 
 --Caso Error: no existe Sucursal
 EXEC [gestion_sucursal].Borrar_Sucursal 9
-
+GO
 
 --Borrar Tipo de Producto
 EXEC [gestion_producto].Borrar_TipoProducto 1
-
+GO
 
 --Borrar Empleado
 EXEC [gestion_sucursal].Borrar_Empleado 1
-
+GO
 --Borrar Producto
 EXEC [gestion_producto].Borrar_Producto 2
-
+GO
 --Caso Error: no existe Medio De Pago
 EXEC gestion_venta.Borrar_MedioDePago 10
-
+GO
 --Borrar Cliente
 EXEC gestion_sucursal.Borrar_Cliente 1
-
---Caso Error: no existe Factura
-EXEC gestion_venta.Borrar_Factura '111-222-9999'
-
+GO
 
